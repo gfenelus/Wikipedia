5ded6aad449fb8f2d2538bda3b73180c310cd865
gfenelus <gfenelus@student.neumont.edu>
2021-02-26 19:24:41 -0700

    name and birthday regex

--- parser2.py
+++ parser2.py
@@ -19,14 +19,23 @@ for ev, el in root:
     # if el.tag == ns + 'text' and 'Actor' in el.text:
     if el.tag == ns + 'text' and re.search(r'occupation \s* = \s*(?:\{{2})?(?:[\w*\|\s?])*(?:\*)?Actor', el.text):
         actorCounter += 1
-        print(title)
+        name = re.search(r'(?:\|?)name \s*=\s*(.*)', el.text)
+
         # print(el.text)
         parsedText = wtp.parse(el.text)
         templates = parsedText.templates
         for template in templates:
             if 'Infobox person' in template:
-                print(template)
-#                 TODO: use regex to pull out data needed for each actor
+                # print(template)
+                #                 TODO: use regex to pull out data needed for each actor
+                name = re.findall(r'(?:\|?)name \s*=\s*(.*)', el.text)
+                bdate_raw = re.findall(r'(?:\|?)birth_date \s*=.*\|(\d{4})\|(\d{1,2})\|(\d{1,2})', el.text)
+                bdate = f"{bdate_raw[0][0]}/{bdate_raw[0][1]}/{bdate_raw[0][2]}"
+
+
+
+        print("name: ", name[0])
+        print("birthdate: ", bdate)
 
 print("Actors found: ", actorCounter)
 print("pages found: ", pageCounter)
